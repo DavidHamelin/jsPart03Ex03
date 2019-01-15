9e971f9991793e5de73ad1507d9df3b12ba5edbc
DavidHamelin <hamelindavid@hotmail.fr>
2019-01-15 10:05:29 +0100

    js ex03 part03

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+﻿function colorGreen() {
+    document.getElementById("text").style.color = "green";
+}
+function colorRed() {
+    document.getElementById("text").style.color = "red";
+}
+function colorBlue() {
+    document.getElementById("text").style.color = "blue";
+}
